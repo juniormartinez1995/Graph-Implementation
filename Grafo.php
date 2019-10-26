f153edcf622ecb872d94052cde07bff3950a4f52
Alfonso Martinez <34066160+juniormartinez1995@users.noreply.github.com>
2019-10-26 17:05:36 -0300

    Organização e comentários

--- Grafo.php
+++ Grafo.php
@@ -1,358 +1,233 @@
 <?php
 
-/**
- * Undirected graph implementation.
- */
 class Graph
 {
-  /**
-   * Adds an undirected edge between $u and $v in the graph.
-   * 
-   * $u,$v can be anything.
-   *
-   * Edge (u,v) and (v,u) are the same.
-   * 
-   * $data is the data to be associated with this edge.
-   * If the edge (u,v) already exists, nothing will happen (the
-   * new data will not be assigned).
-   */
-  public function add_edge($u,$v,$data=null)
-  {
-    assert($this->sanity_check());
-    assert($u != $v);
 
-    if ($this->has_edge($u,$v))
+  private $adj = array();
+
+  private $vertex_data = array();
+
+  private $totalEdges = 0;
+
+  /**
+   * 
+   * Função que adiciona uma aresta, ligada a dois vértices V1 e V2
+   * Caso a aresta já exista, nada acontecerá
+   * Caso o vértice nao exista, o mesmo será criado  
+   */
+  public function addEdge($vertex1,$vertex2,$data=null)
+  {
+
+    if ($this->hasEdge($vertex1,$vertex2))
       return;
 
-    //If u or v don't exist, create them.
-    if (!$this->has_vertex($u))
-      $this->add_vertex($u);
-    if (!$this->has_vertex($v))
-      $this->add_vertex($v);
-
-    //Some sanity.
-    assert(array_key_exists($u,$this->adjacency_list));
-    assert(array_key_exists($v,$this->adjacency_list));
-
-    //Associate (u,v) with data.
-    $this->adjacency_list[$u][$v] = $data;
-    //Associate (v,u) with data.
-    $this->adjacency_list[$v][$u] = $data;
-
-    //We just added two edges
-    $this->edge_count += 2;
-
-    assert($this->has_edge($u,$v));
-
-    assert($this->sanity_check());
-  }
-
-  public function has_edge($u,$v)
-  {
-    assert($this->sanity_check());
-
-    //If u or v do not exist, they surely do not make up an edge.
-    if (!$this->has_vertex($u))
-      return false;
-    if (!$this->has_vertex($v))
-      return false;
+    if (!$this->hasVertex($vertex1))
+      $this->addVertex($vertex1);
+    if (!$this->hasVertex($vertex2))
+      $this->addVertex($vertex2);
 
 
-    //some extra sanity.
-    assert(array_key_exists($u,$this->adjacency_list));
-    assert(array_key_exists($v,$this->adjacency_list));
+    $this->adj[$vertex1][$vertex2] = $data;
+    $this->adj[$vertex2][$vertex1] = $data;
 
-    //This is the return value; if v is a neighbor of u, then its true.
-    $result = array_key_exists($v,$this->adjacency_list[$u]);
+    $this->totalEdges += 2;
 
-    //Make sure that iff v is a neighbor of u, then u is a neighbor of v
-    assert($result == array_key_exists($u,$this->adjacency_list[$v]));
-
-    return $result;
   }
 
   /**
-   * Remove (u,v) and return data.
+   * 
+   * Função que checa se existe uma aresta entra o vértice V1 e V2
+   * Retorna verdadeiro ou falso de acordo com a condição
    */
-  public function remove_edge($u,$v)
+  public function hasEdge($vertex1,$vertex2)
   {
-    assert($this->sanity_check());
 
-    if (!$this->has_edge($u,$v))
+    if (!$this->hasVertex($vertex1)) return false;
+    if (!$this->hasVertex($vertex2)) return false;
+
+
+    return array_key_exists($vertex2,$this->adj[$vertex1]);;
+  }
+
+  /**
+   * Função que remove uma aresta do vértice V1 e V2
+   * Retorna o dado que estava contido nela
+   */
+  public function removeEdge($vertex1,$vertex2)
+  {
+
+    if (!$this->hasEdge($vertex1,$vertex2))
       return null;
 
-    assert(array_key_exists($u,$this->adjacency_list));
-    assert(array_key_exists($v,$this->adjacency_list));
-    assert(array_key_exists($v,$this->adjacency_list[$u]));
-    assert(array_key_exists($u,$this->adjacency_list[$v]));
+    $data = $this->adj[$vertex1][$vertex2];
 
-    //remember data.
-    $data = $this->adjacency_list[$u][$v];
+    unset($this->adj[$vertex1][$vertex2]);
+    unset($this->adj[$vertex2][$vertex1]);
 
-    unset($this->adjacency_list[$u][$v]);
-    unset($this->adjacency_list[$v][$u]);
-
-    //We just removed two edges.
-    $this->edge_count -= 2;
-
-    assert($this->sanity_check());
+    $this->totalEdges -= 2;
 
     return $data;
   }
 
-  //Return data associated with (u,v)
-  public function get_edge_data($u,$v)
+  /**
+   * Função que retorna os dados que estava associado a aresta dos vértices (V1, v2)
+   */
+  public function getEdgeData($vertex1,$vertex2)
   {
-    assert($this->sanity_check());
 
-    //If no such edge, no data.
-    if (!$has_edge($u,$v))
+    if (!$hasEdge($vertex1,$vertex2))
       return null;
 
-    //some sanity.
-    assert(array_key_exists($u,$this->adjacency_list));
-    assert(array_key_exists($v,$this->adjacency_list[$u]));
-
-
-    return $this->adjacency_list[$u][$v]; 
+    return $this->adj[$vertex1][$vertex2]; 
   }
 
   /**
-   * Add a vertex. Vertex must not exist, assertion failure otherwise.
+   * Função que adiciona um vértice e o conteudo contido nele
    */
-  public function add_vertex($u,$data=null)
+  public function addVertex($vertex1,$data=null)
   {
-    assert(!$this->has_vertex($u));
 
-    //Associate data.
-    $this->vertex_data[$u] = $data;
-    //Create empty neighbor array.
-    $this->adjacency_list[$u] = array();
+    $this->vertex_data[$vertex1] = $data;
+    $this->adj[$vertex1] = array();
 
-    assert($this->has_vertex($u));
-    assert($this->sanity_check());
   }
 
-  public function has_vertex($u)
+  /**
+   * Função que checa se o vértice V1 existe
+   */
+  public function hasVertex($vertex1)
   {
-    assert($this->sanity_check());
-    assert(array_key_exists($u,$this->vertex_data) == array_key_exists($u,$this->adjacency_list));
-    return array_key_exists($u,$this->vertex_data);
+    return array_key_exists($vertex1,$this->vertex_data);
   }
 
-  //Returns data associated with vertex, null if vertex does not exist.
-  public function get_vertex_data($u)
+  /**
+   * Função que retorna o conteúdo de um vértice, retorna nulo caso não exista
+   */
+  public function getVertexData($vertex1)
   {
-    assert($this->sanity_check());
 
-    if (!array_key_exists($u,$this->vertex_data))
+    if (!array_key_exists($vertex1,$this->vertex_data))
       return null;
 
-    return $this->vertex_data[$u];
+    return $this->vertex_data[$vertex1];
   }
 
-  //Count the neighbors of a vertex.
-  public function count_vertex_edges($u)
+  /**
+   * Função que conta os vizinhos de um vértice
+   */
+  public function getVertexEdges($vertex1)
   {
-    assert($this->sanity_check());
 
-    if (!$this->has_vertex($u))
+    if (!$this->hasVertex($vertex1))
       return 0;
 
-    //some sanity.    
-    assert (array_key_exists($u,$this->adjacency_list));
-
-    return count($this->adjacency_list[$u]);
+    return count($this->adj[$vertex1]);
   }
 
-  /**
-   * Return an array of neighbor vertices of u.
-   * If $with_data == true, then it will return an associative array, like so:
-   * {neighbor => data}.
-   */
-  public function get_edge_vertices($u,$with_data=false)
-  {
-    assert($this->sanity_check());
 
-    if (!array_key_exists($u,$this->adjacency_list))
+  /**
+   * Função que retorna um array de vizinhos do vértice V1
+   * Caso o vértice contenha conteúdo, retorna uma array associativa
+   */
+  public function getEdgeVertex($vertex1,$with_data=false)
+  {
+
+    if (!array_key_exists($vertex1,$this->adj))
       return array();
 
     $result = array();
 
     if ($with_data) {
-      foreach( $this->adjacency_list[$u] as $v=>$data)
+      foreach( $this->adj[$vertex1] as $vertex2=>$data)
       {
-        $result[$v] = $data;
+        $result[$vertex2] = $data;
       }
     } else {
 
-      foreach( $this->adjacency_list[$u] as $v=>$data)
+      foreach( $this->adj[$vertex1] as $vertex2=>$data)
       {
-        array_push($result, $v);
+        array_push($result, $vertex2);
       }
     }
 
     return $result;
   }
 
-  //Removes a vertex if it exists, and returns its data, null otherwise.
-  public function remove_vertex($u)
+  /**
+   * Função que remove um vértice e retorna o conteúdo contido nele
+   */
+  public function removeVertex($vertex1)
   {
-    assert($this->sanity_check());
 
-    //If the vertex does not exist,
-    if (!$this->has_vertex($u)){
-      //Sanity.
-      assert(!array_key_exists($u,$this->vertex_data));
-      assert(!array_key_exists($u,$this->adjacency_list));
+    if (!$this->hasVertex($vertex1)){
+
       return null;
     }
 
-    //We need to remove all edges that this vertex belongs to.
-    foreach ($this->get_edge_vertices($u) as $v)
+    foreach ($this->getEdgeVertex($vertex1) as $vertex2)
     {
-      $this->remove_edge($u,$v);
+      $this->removeEdge($vertex1,$vertex2);
     }
 
 
-    //After removing all such edges, u should have no neighbors.
-    assert($this->count_vertex_edges($u) == 0);
+    $data = $this->vertex_data[$vertex1];
 
-    //sanity.
-    assert(array_key_exists($u,$this->vertex_data));
-    assert(array_key_exists($u,$this->adjacency_list));
-
-    //remember the data.
-    $data = $this->vertex_data[$u];
-
-    //remove the vertex from the data array.
-    unset($this->vertex_data[$u]);
-    //remove the vertex from the adjacency list.
-    unset($this->adjacency_list[$u]);
-
-    assert($this->sanity_check());
+    unset($this->vertex_data[$vertex1]);
+    unset($this->adj[$vertex1]);
 
     return $data;
   }
-
-  public function get_vertex_count()
+  
+  /**
+   * Função que retorna o número total de vértices daquele grafo
+   */
+  public function getTotalVertex()
   {
-    assert($this->sanity_check());
+
     return count($this->vertex_data);
   }
-  public function get_edge_count()
-  {
-    assert($this->sanity_check());
 
-    //edge_count counts both (u,v) and (v,u)
-    return $this->edge_count/2;
+    /**
+   * Função que retorna o número total de arestas daquele grafo
+   */
+  public function getTotalEdges()
+  {
+    return $this->totalEdges/2;
   }
 
-  public function get_vertex_list($with_data=false)
+  /**
+   * Função que retorna uma lista dos vértices contidos no grafo
+   */
+  public function getVertexList($with_data=false)
   {
     $result = array();
 
     if ($with_data)
-      foreach ($this->vertex_data as $u=>$data)
-        $result[$u]=$data;
+      foreach ($this->vertex_data as $vertex1=>$data)
+        $result[$vertex1]=$data;
     else
-      foreach ($this->vertex_data as $u=>$data)
-        array_push($result,$u);
+      foreach ($this->vertex_data as $vertex1=>$data)
+        array_push($result,$vertex1);
 
     return $result;
   }
 
-
-  public function edge_list_str_array($ordered=true)
+  /**
+   * Função que retorna uma lista das arestas contidas no grafo
+   */
+  public function getEdgeList($ordered=true)
   {
     $result_strings = array();
-    foreach($this->vertex_data as $u=>$udata)
+    foreach($this->vertex_data as $vertex1=>$v1_data)
     {
-      foreach($this->adjacency_list[$u] as $v=>$uv_data)
+      foreach($this->adj[$vertex1] as $vertex2=>$v1v2_data)
       {
-        if (!$ordered || ($u < $v))
-          array_push($result_strings, '('.$u.','.$v.')');
+        if (!$ordered || ($vertex1 < $vertex2))
+          array_push($result_strings, '('.$vertex1.','.$vertex2.')');
       }
     }
     return $result_strings;
   }
 
-  public function sanity_check()
-  {
-    if (count($this->vertex_data) != count($this->adjacency_list))
-      return false;
-
-    $edge_count = 0;
-
-    foreach ($this->vertex_data as $v=>$data)
-    {
-
-      if (!array_key_exists($v,$this->adjacency_list))
-        return false;
-
-      $edge_count += count($this->adjacency_list[$v]);
-    }
-
-    if ($edge_count != $this->edge_count)
-      return false;
-
-    if (($this->edge_count % 2) != 0)
-      return false;
-
-    return true;
-  }
-
-  /**
-   * This keeps an array that associates vertices with their neighbors like so:
-   *
-   * {<vertex> => {<neighbor> => <edge data>}}
-   *
-   * Thus, each $adjacency_list[$u] = array( $v1 => $u_v1_edge_data, $v2 => $u_v2_edge_data ...)
-   *
-   * The edge data can be null.
-   */
-  private $adjacency_list = array();
-
-  /**
-   * This associates each vertex with its data.
-   *
-   * {<vertex> => <data>}
-   *
-   * Thus each $vertex_data[$u] = $u_data
-   */
-  private $vertex_data = array();
-
-  /**
-   * This keeps tracks of the edge count so we can retrieve the count in constant time,
-   * instead of recounting. In truth this counts both (u,v) and (v,u), so the actual count
-   * is $edge_count/2.
-   */
-  private $edge_count = 0;
 }
-
-
-$G = new Graph();
-
-for ($i=0; $i<5; ++$i)
-{
-  $G->add_vertex($i);
-}
-
-for ($i=5; $i<10; ++$i)
-{
-  $G->add_edge($i,$i-5);
-}
-
-print 'V: {'.join(', ',$G->get_vertex_list())."}\n";
-print 'E: {'.join(', ',$G->edge_list_str_array())."}\n";
-
-$G->remove_vertex(1);
-
-print 'V: {'.join(', ',$G->get_vertex_list())."}\n";
-print 'E: {'.join(', ',$G->edge_list_str_array())."}\n";
-
-$G->remove_vertex(1);
-
-print 'V: {'.join(', ',$G->get_vertex_list())."}\n";
-print 'E: {'.join(', ',$G->edge_list_str_array())."}\n";
 ?>
